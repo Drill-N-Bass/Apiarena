50e41191de6638c92ee250e45a6490256c3b1069
drill_n_bass <77461241+Drill-N-Bass@users.noreply.github.com>
2022-08-29 23:35:07 +0200

    Adding dynamic links to pages and templates

--- src/pawel_pedryc_developer/views.py
+++ src/pawel_pedryc_developer/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # Create your views here.
 
 
 def home_view_pawel(request):
+    essay = [
+        {
+            'title':       'Learning Python is a long run, not a sprint',
+            'description': 'Intro do eseju',
+            'slug':        'Learning-Python-is-a-long-run-not-a-sprint',
+            'pict':        'C:/Users/b2b/Desktop/pikachu z png na jpeg.jpg',
+            'location':    'Powidz'
+        },
+        {
+            'title':       'Jakiś drugi artykuł, zapchaj dziura - może dodatkowa część tego mojego pierwszego',
+            'description': 'treść artykułu',
+            'slug':        'The-second-essay',
+            'pict':        'C:/Users/b2b/Desktop/pikachu z png na jpeg.jpg',
+            'location':    'Wrocław'
+        }
+    ]
+
+
     # return HttpResponse('Test')
-    return render(request, 'home_page_pawel_template/pawel_pedryc.html')
+    return render(
+        request,
+        'pawel_pedryc_developer/pawel_pedryc.html',
+        {
+        'text_content': essay,
+        'show_text_content': True
+        })
+
+
+def my_essays(request, home_view_pawel_slug):
+    print("print('home_view_pawel_slug'):", home_view_pawel_slug)
+    selected_essay = {
+        'title':       'A first essay is this',
+        'description': 'this is description'
+        }
+
+    return render(
+        request,
+        'pawel_pedryc_developer/learning-programming-is-a-long-run-not-a-sprint.html',
+        {
+        'essay_title': selected_essay['title'],
+        'essay_description': selected_essay['description']
+        })
