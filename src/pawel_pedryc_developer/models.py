79d9b924390609812f5f16ccacba1a326da25ef4
drill_n_bass <77461241+Drill-N-Bass@users.noreply.github.com>
2022-09-05 15:44:32 +0200

    test 5: gitignore

--- src/pawel_pedryc_developer/models.py
+++ src/pawel_pedryc_developer/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 # Create your models here.
 
-
 class EssayCls(models.Model):
     title = models.CharField(max_length=200)
     slug  = models.SlugField(unique=True) 
